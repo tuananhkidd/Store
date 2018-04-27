e00b3066115b5ea8d34c42a4ddc5a8934204786b
worksfit.dev.team@gmail.com <worksfit.dev.team@gmail.com>
2018-04-27 22:32:36 +0700

    upe

--- app/src/main/java/com/kidd/store/view/cart/CartActivity.java
+++ app/src/main/java/com/kidd/store/view/cart/CartActivity.java
@@ -3,6 +3,8 @@ package com.kidd.store.view.cart;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.kidd.store.R;
+
 public class CartActivity extends AppCompatActivity {
 
     @Override
